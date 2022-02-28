112ef59b257b2eaaf24c73e7c809dfacdd65d7a2
Duany Dreyton <duanyrf@gmail.com>
2022-02-28 13:43:47 -0300

    add test for a new user creation

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "User Test",
+      email: "user@test.com",
+      password: "12345"
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+});
